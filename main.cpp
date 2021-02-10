0bcef6ae5b4895119d627c0b69fda7736b632883
unknown <mkiricenko289@gmail.com>
2021-02-10 18:06:19 +0200

    aditional operator functions

--- main.cpp
+++ main.cpp
@@ -3,26 +3,43 @@
 using namespace std;
 
 class Fraction {
-	private:
-		int _a;
-		int _b;
-	public:
-		Fraction() { _a = _b = 1; }
-		Fraction(int a, int b) { _a = a; _b = b; }
-		~Fraction() {};
-		void setA(int a) { _a = a; }
-		void setB(int b) { _b = b; }
-		int setA() { return _a; }
-		int setB() { return _b; }
-		int getGCD(int b1, int b2);
-		int getLCM(int b1, int b2);
-		void toString() { cout << _a << "/" << _b << endl; }
-		void reduction();
-		Fraction operator*(Fraction obj);
-		Fraction operator/(Fraction obj);
-		Fraction operator+(Fraction obj);
-		Fraction operator-(Fraction obj);
+private:
+	int _a;
+	int _b;
+public:
+	Fraction() { _a = _b = 1; }
+	Fraction(int a, int b);
+	void setA(int a) { _a = a; }
+	void setB(int b) { _b = b; }
+	int setA() { return _a; }
+	int setB() { return _b; }
+	int getGCD(int b1, int b2);
+	int getLCM(int b1, int b2);
+	void toString() { cout << _a << "/" << _b << endl; }
+	void reduction();
+	Fraction operator*(Fraction obj);
+	Fraction operator/(Fraction obj);
+	Fraction operator+(Fraction obj);
+	Fraction operator+(int num);
+	friend Fraction operator+(int num, Fraction obj);
+	Fraction operator-(Fraction obj);
+	bool operator==(Fraction obj);
+	bool operator>(Fraction obj);
+	double toDouble() { return (double)_a / (double)_b; }
 };
+
+Fraction::Fraction(int a, int b)
+{
+	if (a < 0 && b < 0) {
+		_a = -a; _b = -b;
+		return;
+	}
+	else if (b < 0) {
+		_a = -a; _b = -b;
+		return;
+	}
+	_a = a; _b = b;
+}
 
 int Fraction::getGCD(int b1, int b2)
 {
@@ -78,9 +95,63 @@ Fraction Fraction::operator+(Fraction obj)
 	return tmp;
 }
 
-Fraction Fraction::operator$(Fraction obj)
+Fraction Fraction::operator+(int num)
 {
-	return Fraction();
+	Fraction tmp;
+	tmp._b = this->_b;
+	int a = num * tmp._b;
+	tmp._a = a + this->_a;
+	return tmp;
+}
+
+Fraction Fraction::operator-(Fraction obj)
+{
+	Fraction tmp;
+	int commonB = getLCM(this->_b, obj._b);
+	int a1 = commonB / this->_b * this->_a;
+	int a2 = commonB / obj._b * obj._a;
+	tmp._a = a1 - a2;
+	tmp._b = commonB;
+	return tmp;
+}
+
+bool Fraction::operator==(Fraction obj)
+{
+	Fraction tmp1 = *this;
+	tmp1.reduction();
+	Fraction tmp2 = obj;
+	tmp2.reduction();
+	if (tmp1._b == tmp2._b && tmp1._a == tmp2._a)
+		return true;
+	else
+		return false;
+}
+
+bool Fraction::operator>(Fraction obj)
+{
+	Fraction tmp1 = *this;
+	tmp1.reduction();
+	Fraction tmp2 = obj;
+	tmp2.reduction();
+	int commonB = getLCM(this->_b, obj._b);
+	int a1 = commonB / this->_b * this->_a;
+	int a2 = commonB / obj._b * obj._a;
+	tmp1._b = tmp2._b = commonB;
+	tmp1._a = a1;
+	tmp1._a = a2;
+	if (tmp1._a > tmp2._b)
+		return true;
+	else
+		return false;
+}
+
+Fraction operator+(int num, Fraction obj)
+{
+	Fraction tmp;
+	tmp._b = obj._b;
+	int a = num * tmp._b;
+	tmp._a = a + obj._a;
+	return tmp;
 }
 
 int main() {
@@ -88,15 +159,40 @@ int main() {
 	Fraction b(6, 8);
 	a.toString();
 	b.toString();
+	cout << " this is primer " << endl;
 	Fraction c = a * b;
 	c.toString();
 	c.reduction();
 	c.toString();
+	cout << " this is * " << endl;
 	c = a / b;
 	c.toString();
 	c.reduction();
 	c.toString();
+	cout << " this is / " << endl;
 	c = a + b;
 	c.toString();
+	c.reduction();
+	c.toString();
+	cout << " this is + " << endl;
+	c = a - b;
+	c.toString();
+	c.reduction();
+	c.toString();
+	cout << " this is - " << endl;
+	Fraction d(3, 5);
+	d.toString();
+	if (a > d) {
+		cout << "Grater" << endl;
+	}
+	else {
+		cout << "Leser" << endl;
+	}
+	Fraction f(2, 3);
+	//c = f + 2;
+	//c.toString();
+	c = 2 + f;
+	c.toString();
+	cout << c.toDouble() << endl;
 	return 0;
 }
